71696f71b07727fc00fee4a9fc764f87404c39e4
Guido Sterbini <guido.sterbini@cern.ch>
2023-07-25 21:53:11 +0200

    Minor

--- make_it.sh
+++ make_it.sh
@@ -2,6 +2,6 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
 source miniforge/bin/activate                                                    
 python -m pip install ipython jupyterlab numpy scipy pandas awkward matplotlib               
-python -m pip install pyarrow pyyaml  pytest  cpymad   xsuite  pyabel                                          
-python -m pip install sixtracktools                                                
+python -m pip install pyarrow pyyaml pytest cpymad  xsuite pyabel                                          
+# python -m pip install sixtracktools                                                
 python -m pip install NAFFlib   
