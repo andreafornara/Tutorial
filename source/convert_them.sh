452445053bdba6be09c495a10d707c5a40b724d0
Guido Sterbini <guido.sterbini@cern.ch>
2023-07-26 11:58:50 +0200

    Status

--- source/convert_them.sh
+++ source/convert_them.sh
@@ -1,11 +1,11 @@
 # convert all python files in the folder in ipynb using ipynb-py-convert
 for f in *.py; do
-    ipynb-py-convert $f ../examples/${f%.*}.ipynb &
+    ipynb-py-convert $f ../examples/${f%.*}.ipynb
 done
 
 
 # for all ipnb in ../examples/ run them
 # Path: source/convert_them.sh
 for f in ../examples/*.ipynb; do
-    jupyter nbconvert --to notebook --execute $f --output $f &
+    jupyter nbconvert --to notebook --execute $f --output $f
 done
