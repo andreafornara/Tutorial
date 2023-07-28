db9084f07281af70d6f0dd4f985f31b0e10408a7
andrew960 <andrew960@hotmail.it>
2023-07-28 17:31:21 +0200

    survey ready

--- source/003_thick_study.py
+++ source/003_thick_study.py
@@ -3,8 +3,24 @@ import xtrack as xt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import xpart as xp
 import yaml
+import matplotlib.patches as patches
+
+
+def plotLatticeSeries(ax, twiss, element_name, series, height=1., v_offset=0., color='r',alpha=0.5):
+    aux=series
+    ax.add_patch(
+    patches.Rectangle(
+        (twiss_b1['s', element_name]-aux['length']*0, v_offset-height/2.),   # (x,y)
+        aux['length'],          # width
+        height,          # height
+        color=color, alpha=alpha
+    )
+    )
+    return;
+
 
 # %%
 collider = xt.Multiline.from_json('../data/collider.json')
@@ -13,6 +29,7 @@ collider.build_trackers()
 # %%
 twiss_b1 = collider['lhcb1'].twiss()
 twiss_b2 = collider['lhcb2'].twiss().reverse()
+my_dict = collider.lhcb1.to_dict()
 
 # %%
 fig, ax = plt.subplots(2,1)
@@ -30,80 +47,140 @@ ax[1].grid()
 plt.tight_layout()
 
 # %%
-import matplotlib.patches as patches
 
-def plotLatticeSeries(ax, twiss, element_name, series, height=1., v_offset=0., color='r',alpha=0.5):
-    aux=series
-    ax.add_patch(
-    patches.Rectangle(
-        (twiss_b1['s', element_name]-aux['length']*0, v_offset-height/2.),   # (x,y)
-        aux['length'],          # width
-        height,          # height
-        color=color, alpha=alpha
-    )
-    )
-    return;
-
-
-# %%
-# twiss_b1[:, ['s.arc.34.b1', 'e.arc.34.b1']]
-fig = plt.figure(figsize=(20,10))
-
-# set up subplot grid
-#gridspec.GridSpec(3,3)
+fig = plt.figure(figsize=(30,20))
 fontsize = 10
 ax1 = plt.subplot2grid((3,3), (0,0), colspan=3, rowspan=1)
-plt.plot(twiss_b1.rows['s.arc.12.b1':'e.arc.12.b1', ]['s'],0*twiss_b1.rows['s.arc.12.b1':'e.arc.12.b1', ]['s'],'k')
-# plt.plot(twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'][0:300],twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['x'][0:300]*1e4,'green',label='x')
-
-my_dict = collider.lhcb1.to_dict()
-for ii in (twiss_b1.rows['s.arc.12.b1':'e.arc.12.b1', ]['name']):
+plt.plot(twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'],
+         0*twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'],'k')
+for ii in (twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['name']):
     if((ii.startswith('mq.')) and ii.endswith('b1')):
-        # print(ii)
         aux =my_dict['elements'][ii]
         k1l = my_dict['elements'][ii]['k1']*my_dict['elements'][ii]['length']
+        #print(ii, k1l)
         plotLatticeSeries(plt.gca(),twiss_b1, ii, aux, height=k1l, v_offset=k1l/2, color='red')
-        # if(ii == 'mq.14r3.b1'):
-        #     break
+        # add a label for each quadrupole on top of it with its name
+        name = ' '+ii.split('.')[1]+' '
+        plt.gca().text(twiss_b1['s', ii]+28.6, np.abs(k1l)/4, 
+                       name, fontsize=7, color='k', horizontalalignment='center', verticalalignment='center')
 ax1.set_ylabel(r'$K1L$ [1/m]', color='red')  # we already handled the x-label with ax1
 ax1.tick_params(axis='y', labelcolor='red')
-
-FODO_cell_length = twiss_b1['s','mq.14r1.b1']-twiss_b1['s','mq.12r1.b1']
-fodo_cell = patches.Rectangle(( twiss_b1['s','mq.14r1.b1'], 0),
-                              FODO_cell_length, 1, linewidth=1, edgecolor='green', facecolor='green')
-
-#ax1.set_xlim(twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'][0],600)
-
+FODO_cell_length = twiss_b1['s','mq.28l2.b1']-twiss_b1['s','mq.30l2.b1']
 ax2 = ax1.twinx() 
 ax2.set_ylim(-10,10)
-for ii in (twiss_b1.rows['s.arc.12.b1':'e.arc.12.b1', ]['name']):
+for ii in (twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['name']):
     if((ii.startswith('mb.')) and ii.endswith('b1')):
-        # print(ii)
         aux =my_dict['elements'][ii]
         kl = my_dict['elements'][ii]['k0']*my_dict['elements'][ii]['length']
         plotLatticeSeries(plt.gca(),twiss_b1, ii, aux, height=kl*1000, v_offset=kl/2*1000, color='blue')
-        # if(ii == 'mb.c15r3.b1'):
-        #     break
-
-plt.gca().add_patch(fodo_cell)
 color = 'blue'
 ax2.set_ylabel(r'$\theta$=K0L [mrad]', color=color)  # we already handled the x-label with ax1
 ax2.tick_params(axis='y', labelcolor=color)
-plt.title('LHC lattice in the arc 12', fontsize = fontsize)
+plt.title('LHC lattice in the ARC 34', fontsize = fontsize)
 plt.xlabel('s [m]', fontsize = fontsize)
+color = 'k'
+ax3 = ax1.twinx() 
+ax3.set_ylim(-200,200)
+ax3.spines.right.set_position(("axes", 1.05))
+plt.gca().plot(twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'],
+               twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['betx'],'k',label=r'$\beta_{x}$ function')
+plt.gca().plot(twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['s'],
+               twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['bety'],'green',label=r'$\beta_{y}$ function')
 
+ax3.set_ylabel(r'$\beta_{x,y}$ [m]', color=color)  # we already handled the x-label with ax1
+ax3.tick_params(axis='y', labelcolor=color)
 plt.legend(fontsize = fontsize)
-
-plt.grid()
 
 #%%
 
-# #calculate FODO cell length
-# for ii in (twiss_b1.rows['s.arc.12.b1':'e.arc.12.b1', ]['name']):
-#     if((ii.startswith('mq.')) and ii.endswith('b1')):
-#         print(ii)
+x= np.arange(0,4.01,0.01)
+y=2*np.arcsin(x/4)/np.pi
+FODO_cell_length = twiss_b1['s','mq.28l2.b1']-twiss_b1['s','mq.30l2.b1']
+LHCFODO = np.abs(k1l)*FODO_cell_length
+y_LHC = 2*np.arcsin(LHCFODO/4)/np.pi
+fig, ax1 = plt.subplots()
+ax1.plot(x,y,'-')
+ax1.set_ylabel("$\Delta \mu / \pi [rad]$", fontsize=16)
+ax1.set_xlabel("$K*L_{quad}*L_{cell}$ [-]", fontsize=16)
+ax1.grid()
+ax1.tick_params(axis='both', labelsize=16)
+phase_advance = np.abs((twiss_b1['mux','mq.30l2.b1']-twiss_b1['mux','mq.28l2.b1']))
+ax1.plot(LHCFODO, y_LHC, '*', markersize=10, color='red', label='LHC FODO cell')
+#find the point corresponding to y = 0.4
+ax1.set_title('Relation between phase advance and FODO cell properties', fontsize=16)
+ax1.legend(fontsize=10)
+plt.tight_layout()
+
+
+
+
+
+
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#collider.vars['kqt12.r3b1']._find_dependant_targets()
+#collider.vars['kqtd.b1']._find_dependant_targets()
+
+
+# %%
+# for ii in (twiss_b1.rows['s.arc.34.b1':'e.arc.34.b1', ]['name']):
+#     if((ii.startswith('mqt.')) and ii.endswith('b1')):
 #         aux =my_dict['elements'][ii]
 #         k1l = my_dict['elements'][ii]['k1']*my_dict['elements'][ii]['length']
-#         print(k1l)
+#         print(ii, k1l)
+# # %%
+# start_index = collider['lhcb1'].element_names.index('s.arc.34.b1')
+# end_index = collider['lhcb1'].element_names.index('e.arc.34.b1')
+# FODO_cell = xt.Line(
+#     elements=(collider['lhcb1'].element_dict),
+#     element_names=[ii for ii in collider['lhcb1'].element_names][start_index:end_index+1])
+# FODO_cell.build_tracker()
 
+# # %%
+# ref = twiss_b1['particle_on_co']
+# twiss_FODO = FODO_cell.twiss_default(particle_ref=ref, ele_start = 's.arc.34.b1', 
+#                              ele_stop = 'e.arc.34.b1', twiss_init='preserve', 
+#                              method='4d')
+# # %%
+# ref  = xp.Particles(
+#                     mass0=xp.PROTON_MASS_EV, q0=1, energy0=7000e9, x = 1e-6, y = 1e-6)
+# print('Particle before FODO cell')
+# ref.show()
+# print('Particle after FODO cell')
+# FODO_cell.track(ref)
+# ref.show()
+
+
+
+
+# %%
+# #plot the beta functions in the FODO cell
+# fig, ax = plt.subplots(2,1)
+# fig.set_size_inches(18.5, 10.5)
+# fontsize = 20
+# fig.suptitle('Beta functions',fontsize = fontsize )
+# ax[0].plot(twiss_FODO['s'], twiss_FODO['betx'], label = r'$\beta_{x}$')
+# ax[0].set_ylabel(r'$\beta_{x}$ [m]', fontsize = fontsize)
+# ax[0].grid()
+# ax[0].legend(fontsize = fontsize)
+# ax[1].plot(twiss_FODO['s'], twiss_FODO['bety'], label = r'$\beta_{y}$')
+# ax[1].set_ylabel(r'$\beta_{y}$ [m]', fontsize = fontsize)
+# ax[1].set_xlabel('s [m]', fontsize = fontsize)
+# ax[1].grid()
+# plt.tight_layout()
+
+# print(index)
 # %%
--- source/004_survey.py
+++ source/004_survey.py
@@ -9,181 +9,233 @@ import yaml
 import matplotlib.patches as patches
 
 # %%
-collider = xt.Multiline.from_json('../data/collider.json')
-collider.build_trackers()
+'''
+In this example we will look at the `survey` of the collider.
+The survey represents the physical position of the elements in the collider in the laboratory frame.
+We set the origin of the survey at the IP1.
+
+First of all we need to load our collider. 
+This is a json file that contains all the information on the collider.
+Namely it contains the two lines `lhcb1` and `lhcb2` corresponding to the two beams.
+'''
 
 # %%
-twiss_b1 = collider['lhcb1'].twiss()
-twiss_b2 = collider['lhcb2'].twiss().reverse()
+collider = xt.Multiline.from_json('../data/collider.json')
+collider.build_trackers()
 my_dict = collider.lhcb1.to_dict()
+
 # %%
-survey_b1 = collider['lhcb1'].survey()
-survey_b2 = collider['lhcb2'].survey().reverse()
+'''
+Now we want to call the `survey` method, two things are important:
+- We want to set the starting point at IP1;
+- In the MAD-X fashion b2 has a different reference frame w.r.t. b1. 
+To be able to plot the two together we need to `reverse` the survey of the b2. 
+'''
+
 # %%
-#plot the surver x vs z
+survey_b1 = collider['lhcb1'].survey(element0='ip1')
+survey_b2 = collider['lhcb2'].survey(element0='ip1').reverse()
+
+# %%
+'''
+Let's look at the `HL-LHC` machine, in the plot you can notice:
+- The `ARCs` in green, these are the regions where the FODO cells are responsible for curving and focusing the beam;
+- The `Dispersion Suppression` regions in violet, to remove the dispersion caused by the preceeding section;
+- The `Straigh Sections` in red, the regions of the tunnel where the total dipolar contribution is zero;
+- The 4 `IPs` in red, those with a shared vacuum pipe (i.e. the ones where the bunches collide and the experiments are built):
+    - `IP1` : ATLAS experiment;
+    - `IP2` : ALICE experiment;
+    - `IP5` : CMS experiment;
+    - `IP8` : LHCb experiment;
+- The 4 `IPs` in black, the regions where the two bunches do not collide :
+    - `IP3` : Momentum cleaning;
+    - `IP4` : RF and Beam Instrumentation;
+    - `IP6` : Beam dumping system;
+    - `IP7` : Betatron cleaning;
+Now we want to plot the survey of the two beams together.
+'''
+
+# %%
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(8.5, 8.5)
 fontsize = 20
 fig.suptitle('Survey of the LHC lattice for Beam 1 and Beam 2',fontsize = fontsize )
-ax.plot(survey_b1['X'], survey_b1['Z'])  
-#ax.plot(survey_b2['X'], survey_b2['Z'])
+ax.plot(-survey_b1['Z'],-survey_b1['X'], color='red')  
 ip_links = ['12','23','34','45','56','67','78','81']
 for ii in ip_links:
-    ax.plot(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'],
-            survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'],
+    ax.plot(-survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'],
+            -survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'],
             color='green')
-    #write the 'ARC'+ii beside the arc
-    ax.text(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'].mean(),
-            survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'].mean(),
-            'ARC'+ii, fontsize=fontsize, color='green')
+    ax.text(-survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'].mean(),
+            -survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'].mean(),
+           'ARC'+ii, fontsize=fontsize, color='green')
     
 r_ip = ['r1','r2','r3','r4','r5','r6','r7','r8']
 l_ip = ['l1','l2','l3','l4','l5','l6','l7','l8']
 for ii in r_ip:
-    ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
-            survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
-            color='purple')
+    ax.plot(-survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+           -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
+           color='purple')
 
 for ii in l_ip:
-    ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
-            survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+    ax.plot(-survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+            -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
             color='purple')
 
-
-ax.plot(survey_b1.rows['ip1']['X'], survey_b1.rows['ip1']['Z'], '*', markersize=20, color='red')
-ax.text(survey_b1.rows['ip1']['X'], survey_b1.rows['ip1']['Z']-380,
-        'IP1', fontsize=fontsize, color='red')
-ax.plot(survey_b1.rows['ip2']['X'], survey_b1.rows['ip2']['Z'], '*', markersize=20, color='red')
-ax.text(survey_b1.rows['ip2']['X']+300, survey_b1.rows['ip2']['Z'],
-        'IP2', fontsize=fontsize, color='red')
-ax.plot(survey_b1.rows['ip5']['X'], survey_b1.rows['ip5']['Z'], '*', markersize=20, color='red')
-ax.text(survey_b1.rows['ip5']['X'], survey_b1.rows['ip5']['Z']+120,
-        'IP5', fontsize=fontsize, color='red')
-ax.plot(survey_b1.rows['ip8']['X'], survey_b1.rows['ip8']['Z'], '*', markersize=20, color='red')
-ax.text(survey_b1.rows['ip8']['X']-800, survey_b1.rows['ip8']['Z'],
-        'IP8', fontsize=fontsize, color='red')
-#same for ip3,4,6,7 but in black
-ax.plot(survey_b1.rows['ip3']['X'], survey_b1.rows['ip3']['Z'], '*', markersize=20, color='black')
-ax.text(survey_b1.rows['ip3']['X'], survey_b1.rows['ip3']['Z']+120,
-        'IP3', fontsize=fontsize, color='black')
-ax.plot(survey_b1.rows['ip4']['X'], survey_b1.rows['ip4']['Z'], '*', markersize=20, color='black')
-ax.text(survey_b1.rows['ip4']['X'], survey_b1.rows['ip4']['Z']+120,
-        'IP4', fontsize=fontsize, color='black')
-ax.plot(survey_b1.rows['ip6']['X'], survey_b1.rows['ip6']['Z'], '*', markersize=20, color='black')
-ax.text(survey_b1.rows['ip6']['X'], survey_b1.rows['ip6']['Z']+120,
-        'IP6', fontsize=fontsize, color='black')
-ax.plot(survey_b1.rows['ip7']['X'], survey_b1.rows['ip7']['Z'], '*', markersize=20, color='black')
-ax.text(survey_b1.rows['ip7']['X'], survey_b1.rows['ip7']['Z']+120,
-        'IP7', fontsize=fontsize, color='black')    
-ax.set_xlabel('x [m]', fontsize = fontsize)
-ax.set_ylabel('z [m]', fontsize = fontsize)
+ax.plot(-survey_b1.rows['ip1']['Z'],-survey_b1.rows['ip1']['X'],  'o', markersize=5, color='red')
+ax.text(-survey_b1.rows['ip1']['Z'],-survey_b1.rows['ip1']['X'], 'IP1', fontsize=fontsize, color='red')
+ax.plot(-survey_b1.rows['ip2']['Z'],-survey_b1.rows['ip2']['X'],  'o', markersize=5, color='red')
+ax.text(-survey_b1.rows['ip2']['Z']+300,-survey_b1.rows['ip2']['X'], 'IP2', fontsize=fontsize, color='red')
+ax.plot(-survey_b1.rows['ip5']['Z'],-survey_b1.rows['ip5']['X'],  'o', markersize=5, color='red')
+ax.text(-survey_b1.rows['ip5']['Z'],-survey_b1.rows['ip5']['X']+120, 'IP5', fontsize=fontsize, color='red')
+ax.plot(-survey_b1.rows['ip8']['Z'],-survey_b1.rows['ip8']['X'],  'o', markersize=5, color='red')
+ax.text(-survey_b1.rows['ip8']['Z']-800,-survey_b1.rows['ip8']['X'], 'IP8', fontsize=fontsize, color='red')
+ax.plot(-survey_b1.rows['ip3']['Z'],-survey_b1.rows['ip3']['X'],  'o', markersize=5, color='black')
+ax.text(-survey_b1.rows['ip3']['Z'],-survey_b1.rows['ip3']['X'], 'IP3', fontsize=fontsize, color='black')
+ax.plot(-survey_b1.rows['ip4']['Z'],-survey_b1.rows['ip4']['X'],  'o', markersize=5, color='black')
+ax.text(-survey_b1.rows['ip4']['Z']+300,-survey_b1.rows['ip4']['X'], 'IP4', fontsize=fontsize, color='black')
+ax.plot(-survey_b1.rows['ip6']['Z'],-survey_b1.rows['ip6']['X'],  'o', markersize=5, color='black')
+ax.text(-survey_b1.rows['ip6']['Z'],-survey_b1.rows['ip6']['X']+120, 'IP6', fontsize=fontsize, color='black')
+ax.plot(-survey_b1.rows['ip7']['Z'],-survey_b1.rows['ip7']['X'],  'o', markersize=5, color='black')
+ax.text(-survey_b1.rows['ip7']['Z']-800,-survey_b1.rows['ip7']['X'], 'IP7', fontsize=fontsize, color='black')
+  
+ax.set_xlabel('z [m]', fontsize = fontsize)
+ax.set_ylabel('x [m]', fontsize = fontsize)
 ax.grid()
-ax.legend(fontsize = fontsize)
+plt.legend(fontsize = fontsize)
 plt.tight_layout()
 
 # %%
-#zoom in on IP3
+'''
+Let's zoom into IP3, a non colliding IP. We can see how the two beam pipes are well separated.
+Notice: we call `Straight Section` something that is not strictly straight, since we have the 'doglegs'.
+What we mean by 'Straight' is a total dipolar contribution equal to zero in that section.
+
+'''
+
+# %%
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(8.5, 8.5)
 fontsize = 20
-
-ii = '23'
-ax.plot(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'],
-            survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'],
-            color='green')
-# ax.text(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'].mean(),
-#         survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'].mean(),
-#         'ARC'+ii, fontsize=fontsize, color='green')
-
-ii = '34'
-ax.plot(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'],
-            survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'],
-            color='green')
-# ax.text(survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['X'].mean(),
-#         survey_b1.rows['s.arc.'+ii+'.b1':'e.arc.'+ii+'.b1', ]['Z'].mean(),
-#         'ARC'+ii, fontsize=fontsize, color='green')
-
+#-----------------b1-----------------
 ii = 'r3'
-ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
-        survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
-        color='purple')
+ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+        -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
+        color='purple', label='Dispersion Suppressors')
+ax.plot(survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['Z'],
+        -survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['X'],
+        color='violet', label='Dispersion Suppressors b2')
+
 ii = 'l3'
-ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
-        survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+        -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
         color='purple')
-
-#straight section
-ax.plot(survey_b1.rows['ip3':'s.ds.'+'r3'+'.b1', ]['X'],
-        survey_b1.rows['ip3':'s.ds.'+'r3'+'.b1', ]['Z'],
+ax.plot(survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['Z'],
+        -survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['X'],
+        color='violet')
+ax.plot(survey_b1.rows['ip3':'s.ds.'+'r3'+'.b1', ]['Z'],
+        -survey_b1.rows['ip3':'s.ds.'+'r3'+'.b1', ]['X'],
+        color='red', label='Straight Section')
+ax.plot(survey_b1.rows['e.ds.'+'l3'+'.b1':, ]['Z'],
+        -survey_b1.rows['e.ds.'+'l3'+'.b1':, ]['X'],
         color='red')
-ax.plot(survey_b1.rows['e.ds.'+'l3'+'.b1':, ]['X'],
-        survey_b1.rows['e.ds.'+'l3'+'.b1':, ]['Z'],
-        color='red')
-
-
-# survey_b1.rows['s.ds.'+'r3'+'.b1':'e.ds.'+'r3'+'.b1', ]['Z']
-
-
-
-ax.plot(survey_b1.rows['ip3']['X'], survey_b1.rows['ip3']['Z'], '*', markersize=20, color='black')
-plt.xlim(-100,10)
-plt.ylim(-800,800)
+ii = 'r3'
+ax.plot(survey_b2.rows[:'e.ds.'+ii+'.b2', ]['Z'][0:175],
+        -survey_b2.rows[:'e.ds.'+ii+'.b2', ]['X'][0:175],
+        color='lightcoral', label='Straight Section b2')
+ax.plot(survey_b2.rows['e.ds.'+'l3'+'.b2':, ]['Z'],
+        -survey_b2.rows['e.ds.'+'l3'+'.b2':, ]['X'],
+        color='lightcoral')
+ax.plot(survey_b1.rows['ip3']['Z'], -survey_b1.rows['ip3']['X'], 'o', markersize=5, color='black', label='IP3, b1')
+ax.plot(survey_b2.rows['ip3']['Z'], -survey_b2.rows['ip3']['X'], 'o', markersize=5, color='grey', label='IP3, b2')
+plt.title('Zoom in around IP3', fontsize = fontsize)
+plt.legend()
+plt.xlabel('z [m]', fontsize = fontsize)
+plt.ylabel('x [m]', fontsize = fontsize)
+plt.xlim(4204,4205)
+plt.ylim(3750,4650)
 plt.grid()
 
 # %%
-fig, ax = plt.subplots(1,1)
-fig.set_size_inches(8.5, 8.5)
-fontsize = 20
-fig.suptitle('Survey of the LHC lattice for Beam 1 and Beam 2',fontsize = fontsize )
-ax.plot(survey_b1.rows[:, 'ip4%%-10': 'ip4%%+10']['X'], survey_b1.rows[:, 'ip4%%-10': 'ip4%%+10']['Z'])
-ax.plot(survey_b2.rows[:, 'ip4%%-25': 'ip4%%+25']['X'], survey_b2.rows[:, 'ip4%%-25': 'ip4%%+25']['Z'])
-
-
-
+'''
+Let's zoom into IP5, a colliding IP where the CMS experiment is located. 
+Notice the following:
+- There is a common vacuum pipe in this case, the interaction is possible;
+- The bunches SWAP the original pipe each round, and this happens at each of the 4 experiments, why? This is crucial to ensure that the two bunches travel the same distance each round.
+- A shared common pipe means interaction between the two bunches even at long distances: this is why here we talk about Long Range Beam Beam together with the Head On interaction happening exactly under IP5.
+'''
 
 # %%
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(8.5, 8.5)
 fontsize = 20
-fig.suptitle('Survey of the LHC lattice for Beam 1 and Beam 2',fontsize = fontsize )
-ax.plot(survey_b1.rows[:, 'ip5%%-100': 'ip5%%+100']['X'], survey_b1.rows[:, 'ip5%%-100': 'ip5%%+100']['Z'])
-ax.plot(survey_b2.rows[:, 'ip5%%-100': 'ip5%%+100']['X'], survey_b2.rows[:, 'ip5%%-100': 'ip5%%+100']['Z'])
+ii = 'r5'
+ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+        -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
+        color='purple', label='Dispersion Suppressors')
+ax.plot(survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['Z'],
+        -survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['X'],
+        color='violet', label='Dispersion Suppressors b2')
+ii = 'l5'
+ax.plot(survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['Z'],
+        -survey_b1.rows['s.ds.'+ii+'.b1':'e.ds.'+ii+'.b1', ]['X'],
+        color='purple')
+ax.plot(survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['Z'],
+        -survey_b2.rows['s.ds.'+ii+'.b2':'e.ds.'+ii+'.b2', ]['X'],
+        color='violet')
+ax.plot(survey_b1.rows['s.ds.'+'l5'+'.b1':'ip5', ]['Z'][310:],
+        -survey_b1.rows['s.ds.'+'l5'+'.b1':'ip5', ]['X'][310:],
+        color='red')
+ax.plot(survey_b1.rows['ip5':'e.ds.'+'r5'+'.b1', ]['Z'][:230],
+        -survey_b1.rows['ip5':'e.ds.'+'r5'+'.b1', ]['X'][:230],
+        color='red', label='Straight Section')
 
+ax.plot(survey_b2.rows['ip5':'e.ds.'+'r5'+'.b2', ]['Z'][:230],
+        -survey_b2.rows['ip5':'e.ds.'+'r5'+'.b2', ]['X'][:230],
+        color='red', label='Straight Section b2')
+ax.plot(survey_b2.rows['ip5':'e.ds.'+'r5'+'.b2', ]['Z'][:75],
+        -survey_b2.rows['ip5':'e.ds.'+'r5'+'.b2', ]['X'][:75],
+        color='k', label='Common Pipe')
 
+ax.plot(survey_b2.rows['s.ds.'+'l5'+'.b2':'ip5', ]['Z'][310:],
+        -survey_b2.rows['s.ds.'+'l5'+'.b2':'ip5', ]['X'][310:],
+        color='red')
+ax.plot(survey_b2.rows['s.ds.'+'l5'+'.b2':'ip5', ]['Z'][495:],
+        -survey_b2.rows['s.ds.'+'l5'+'.b2':'ip5', ]['X'][495:],
+        color='k')
+ax.plot(survey_b2.rows['ip5']['Z'], -survey_b2.rows['ip5']['X'], 'o', markersize=5, color='grey', label='IP5')
+plt.title('Zoom in around IP5', fontsize = fontsize)
+plt.legend()
+plt.xlabel('z [m]', fontsize = fontsize)
+plt.ylabel('x [m]', fontsize = fontsize)
+plt.xlim(-550,550)
+plt.ylim(8409.25,8409.75)
+plt.grid()
 
-#%%
-
-
+# %%
+'''
+Now we can calculate the distance between the two beams at each IP.
+We can see that the distance is zero at the interaction points and it's greater than zero at the other IPs.
+'''
 
 # %%
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(20, 8.5)
 fontsize = 20
-#ax.plot(survey_b1['Z'],survey_b1['X']-survey_b2['X']) 
-ax.plot(survey_b1.rows['ip1']['Z'],survey_b1.rows['ip1']['X']-survey_b2.rows['ip1']['X'], '*', markersize=20, color='red')
-ax.plot(survey_b1.rows['ip2']['Z'],survey_b1.rows['ip2']['X']-survey_b2.rows['ip2']['X'], '*', markersize=20, color='red')
-ax.plot(survey_b1.rows['ip5']['Z'],survey_b1.rows['ip5']['X']-survey_b2.rows['ip5']['X'], '*', markersize=20, color='red')
-ax.plot(survey_b1.rows['ip8']['Z'],survey_b1.rows['ip8']['X']-survey_b2.rows['ip8']['X'], '*', markersize=20, color='red')
-
-ax.plot(survey_b1.rows['ip3']['Z'],survey_b1.rows['ip3']['X']-survey_b2.rows['ip3']['X'], '*', markersize=20, color='k')
-ax.plot(survey_b1.rows['ip4']['Z'],survey_b1.rows['ip4']['X']-survey_b2.rows['ip4']['X'], '*', markersize=20, color='k')
-ax.plot(survey_b1.rows['ip6']['Z'],survey_b1.rows['ip6']['X']-survey_b2.rows['ip6']['X'], '*', markersize=20, color='k')
-ax.plot(survey_b1.rows['ip7']['Z'],survey_b1.rows['ip7']['X']-survey_b2.rows['ip7']['X'], '*', markersize=20, color='k')
-
-print('ip1 difference =',survey_b1.rows['ip1']['X']-survey_b2.rows['ip1']['X'])
-print('ip2 difference =',survey_b1.rows['ip2']['X']-survey_b2.rows['ip2']['X'])
-print('ip5 difference =',survey_b1.rows['ip5']['X']-survey_b2.rows['ip5']['X'])
-print('ip8 difference =',survey_b1.rows['ip8']['X']-survey_b2.rows['ip8']['X'])
-
-print('ip3 difference =',survey_b1.rows['ip3']['X']-survey_b2.rows['ip3']['X'])
-print('ip4 difference =',survey_b1.rows['ip4']['X']-survey_b2.rows['ip4']['X'])
-print('ip6 difference =',survey_b1.rows['ip6']['X']-survey_b2.rows['ip6']['X'])
-print('ip7 difference =',survey_b1.rows['ip7']['X']-survey_b2.rows['ip7']['X'])
-
-
-ax.set_xlabel('z [m]', fontsize = fontsize)
-ax.set_ylabel('x [m]', fontsize = fontsize)
-ax.grid()
-ax.legend(fontsize = fontsize)
-plt.tight_layout()
-# %%
+ips =['ip1','ip2','ip3','ip4','ip5','ip6','ip7','ip8']
+positions_b1 = np.zeros((len(ips),2))
+positions_b2 = np.zeros((len(ips),2))
+for ii in ips:
+        positions_b1[ips.index(ii),0] = survey_b1.rows[ii]['Z']
+        positions_b1[ips.index(ii),1] = survey_b1.rows[ii]['X']
+        positions_b2[ips.index(ii),0] = survey_b2.rows[ii]['Z']
+        positions_b2[ips.index(ii),1] = survey_b2.rows[ii]['X']
+distances = []
+for ii in range(len(ips)):
+        print(ips[ii],np.sqrt((positions_b1[ii,0]-positions_b2[ii,0])**2+(positions_b1[ii,1]-positions_b2[ii,1])**2))
+        distances.append(np.sqrt((positions_b1[ii,0]-positions_b2[ii,0])**2+(positions_b1[ii,1]-positions_b2[ii,1])**2)*100)
+ax.plot(ips,distances,'o',markersize=10)
+plt.title('Distance between IPs', fontsize = fontsize)
+plt.xlabel('IP name', fontsize = fontsize)
+plt.ylabel('Distance [cm]', fontsize = fontsize)
+plt.grid()
