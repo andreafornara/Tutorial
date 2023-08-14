bf513a5a27044e534594d00c137cef9180d47a31
sterbini <guido.sterbini@cern.ch>
2023-08-14 09:45:25 +0200

    Update install_miniforge.sh

--- install_miniforge.sh
+++ install_miniforge.sh
@@ -2,3 +2,4 @@ curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mi
 bash Miniforge3-$(uname)-$(uname -m).sh -b  -p ./miniforge -f
 source miniforge/bin/activate                                                    
 python -m pip install -r requirements.txt
+xsuite-prebuild
